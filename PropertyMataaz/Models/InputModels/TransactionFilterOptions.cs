6825eebb97824b1704f4753e01b9b0160947c256
adelowomi <adelowomi@gmail.com>
2023-03-06 11:24:35 +0100

    fixed transaction list

--- PropertyMataaz/Models/InputModels/TransactionFilterOptions.cs
+++ PropertyMataaz/Models/InputModels/TransactionFilterOptions.cs
@@ -4,8 +4,8 @@ namespace PropertyMataaz.Models.InputModels
 {
     public class TransactionFilterOptions
     {
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public string Status { get; set; }
     }
 }
